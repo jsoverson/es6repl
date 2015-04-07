3f9e6321d456574b864044b5ac28f5729b3cb7d7
Jarrod Overson <joverson@shapesecurity.com>
2015-04-07 10:49:09 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+es6repl
+-------
+
+This is a minimally adapted version of @zenparsing's esdown
+repl intended for use as an iframe for blogging/documentation purposes.
+
+## Usage
+
+* Go to [the repl](http://jsoverson.github.io/es6repl)
+* Type in the code you want to display by default
+* Enter `.link` in the repl
+* Copy the link for distribution or iframe embedding.
+
+## Differences from esdown repl
+
+* Generated links are executed command-by-command vs as a whole program.
+* Styled to be more amenable to embedding.
+
+## License
+
+ESDown license below.
+
+Copyright (c) 2014 zenparsing (Kevin Smith)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ repl.js
@@ -0,0 +1,490 @@
+
+// Majority portion from Kevin Smith's esdown repl http://esparse.org/esdown/repl/
+// esdown license included below.
+
+/**
+ * Copyright (c) 2014 zenparsing (Kevin Smith)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// Adapted from underscore
+var escapeHTML = (function () {
+  "use strict";
+
+  // List of HTML entities for escaping.
+  var escapeMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '`': '&#x60;'
+  };
+
+  // Functions for escaping and unescaping strings to/from HTML interpolation.
+  function createEscaper(map) {
+
+    function escaper(match) {
+      return map[match]
+    }
+
+    // Regexes for identifying a key that needs to be escaped
+    var source = '(?:' + Object.keys(map).join('|') + ')';
+    var testRegexp = RegExp(source);
+    var replaceRegexp = RegExp(source, 'g');
+
+    return function (string) {
+      string = string == null ? '' : '' + string;
+      return testRegexp.test(string) ? string.replace(replaceRegexp, escaper) : string;
+    };
+  }
+
+  return createEscaper(escapeMap);
+
+})();
+
+function replEval() {
+  return window.eval(arguments[0])
+}
+
+window.onload = function () {
+  "use strict";
+
+  var HELP_TEXT = [
+
+    "",
+    "[esdown v" + _esdown.version + "]",
+    "",
+    "This REPL will translate ES6+ syntax and execute the resulting " +
+    "code in your current JavaScript engine.",
+    "",
+    "The following REPL commands are supported:",
+    "",
+    ".help                  Display this help message",
+    ".clear                 Clear the terminal output",
+    ".load                  Load a script from a URL",
+    ".translate             Translate script code to ES5",
+    ".translateModule       Translate module code to ES5",
+    ".parse                 Parse script code",
+    ".parseModule           Parse module code",
+    "\n",
+
+  ].join("\n");
+
+  var MAX_CONSOLE_LINES = 100,
+    NO_OUTPUT = {},
+    NEWLINE = /\r\n?|\n/g;
+
+  function Literal(text) {
+    this.text = text
+  }
+
+  var replCommands = {
+
+    help: function () {
+      return new Literal(HELP_TEXT)
+    },
+    clear: function () {
+      return clearLines(1), NO_OUTPUT
+    },
+    translate: function (code) {
+      return new Literal(esdown.translate(code))
+    },
+    translateModule: function (code) {
+      return new Literal(esdown.translate(code, {module: true}))
+    },
+    parse: function (code) {
+      return esdown.parse(code).ast
+    },
+    parseModule: function (code) {
+      return esdown.parse(code, {module: true}).ast
+    },
+    link: function () {
+      return new Literal(generateLink())
+    }
+  };
+
+  var history = {
+    list: [""],
+    max: 50,
+    current: 0,
+    add: function (str) {
+      var len = this.list.length;
+      this.list[len - 1] = str;
+      this.list.push("");
+      this.current = len;
+    },
+    back: function (str) {
+      this._check(str);
+      if (this.current > 0) this.current -= 1;
+      return this.list[this.current];
+    },
+
+    forward: function (str) {
+      this._check(str);
+      if (this.current < this.list.length - 1) this.current += 1;
+      return this.list[this.current];
+    },
+
+    _check: function (str) {
+      if (str !== this.list[this.current]) {
+        this.current = this.list.length - 1;
+        this.list[this.current] = str;
+      }
+    }
+  };
+
+  function isOpaque(obj) {
+    return obj instanceof Node;
+  }
+
+  function elem(s) {
+    return document.querySelector(s);
+  }
+
+  function elems(s) {
+    return document.querySelectorAll(s);
+  }
+
+  function stylize(str, styleType) {
+    str = escapeHTML(str);
+    if (styleType) str = '<span class="js-' + styleType + '">' + str + '</span>';
+    return str;
+  }
+
+  function isRecoverableError(e, code) {
+    if (/(\n[ \t]*){2}$/.test(code)) return false;
+
+    var pattern = /^(Unexpected end of input|Unexpected token )/;
+    return e && e.name === 'SyntaxError' && pattern.test(e.message);
+  }
+
+  function formatError(error) {
+
+    var message = "" + error,
+      stack = message;
+
+    if ("stack" in error) {
+
+      stack = error.stack;
+
+      var m = /(^|\s)replEval[:@\s]/.exec(stack);
+
+      if (m) {
+
+        var start = Math.max(stack.lastIndexOf("\n", m.index + 1), 0);
+        stack = stack.slice(0, start);
+      }
+
+      if (stack.indexOf(message) < 0)
+        stack = message + "\n" + stack;
+    }
+
+    return escapeHTML(stack).replace(/^.+/, function (m) {
+
+      return "<span class='js-error'>" + m + "</span>";
+    });
+  }
+
+  function replRun() {
+    var code = input.value;
+
+    if (bufferedInput)
+      code = bufferedInput + "\n" + code;
+
+    advanceInput(function () {
+      var executed = false,
+        output = "",
+        result,
+        error;
+
+      if (code.charAt(0) === ".") {
+        var cmd = code.slice(1).replace(/\s[\s\S]*/g, "");
+        if (typeof replCommands[cmd] === "function") {
+          executed = true;
+          try {
+            result = replCommands[cmd](code.slice(cmd.length + 1).trim())
+          }
+          catch (x) {
+            error = x || {}
+          }
+        }
+      }
+
+      if (!executed) {
+        executed = true;
+        try {
+          code = esdown.translate(code);
+          if (code.indexOf("*") >= 0 && window.regenerator) code = regenerator.compile(code).code;
+          result = replEval(code);
+        } catch (x) {
+          error = x || {};
+        }
+      }
+
+      if (isRecoverableError(error, code)) {
+        bufferedInput = code;
+      } else if (result !== NO_OUTPUT) {
+        bufferedInput = "";
+        output =
+          error ? formatError(error) :
+            result instanceof Literal ? escapeHTML(result.text) :
+              prettyPrint(result, {stylize: stylize, isOpaque: isOpaque});
+      }
+      return output;
+
+    });
+  }
+
+  function autoIndent(last) {
+    var indent = last.split(NEWLINE).pop().replace(/\S[\s\S]*/, "");
+    if (/[\{\[]\s*$/.test(last)) indent += "  ";
+    return indent;
+  }
+
+  function advanceInput(fn) {
+
+    var value = input.value;
+
+    history.add(value);
+
+    addLine(escapeHTML(value), prompt.className);
+    setInputValue("");
+
+    var output = fn && fn() || "";
+
+    if (output)
+      addLine(output);
+
+    prompt.className = bufferedInput ? "prompt cont" : "prompt";
+
+    clearLines(MAX_CONSOLE_LINES);
+
+    if (bufferedInput)
+      setInputValue(autoIndent(value));
+
+    input.focus();
+    input.scrollIntoView();
+  }
+
+  function clearLines(max) {
+
+    var list = terminal.getElementsByTagName("div");
+
+    for (var count = list.length; count-- > max;)
+      terminal.removeChild(list[0]);
+  }
+
+  function addLine(html, promptClass) {
+    var line = document.createElement("div");
+    line.className = "output-line";
+    line.innerHTML = html || " ";
+
+    if (promptClass) {
+
+      var span = document.createElement("span");
+      span.className = promptClass;
+      line.insertBefore(span, line.firstChild);
+      line.className += " echo";
+    }
+
+    terminal.insertBefore(line, input.parentNode);
+  }
+
+  function abort() {
+    bufferedInput = "";
+    advanceInput();
+  }
+
+  var resizeTimer = 0;
+
+  function resizeInput() {
+
+    if (resizeTimer)
+      return;
+
+    resizeTimer = setTimeout(function () {
+
+      resizeTimer = 0;
+
+      var value = input.value;
+
+      if (value === "") {
+
+        input.style.height = "auto";
+
+      } else {
+
+        hidden.style.width = input.scrollWidth;
+        hidden.value = value;
+        input.style.height = (hidden.scrollHeight + hidden.clientHeight) + "px";
+      }
+
+    }, 50);
+  }
+
+  function onKeyPress(evt) {
+    // Enter
+    if (evt.keyCode === 13) {
+      if (!evt.shiftKey && !evt.ctrlKey) {
+        replRun();
+        evt.preventDefault();
+        return;
+      }
+    }
+    resizeInput();
+  }
+
+  function setInputValue(value) {
+
+    input.value = value;
+    input.selectionStart = value.length;
+    input.selectionEnd = value.length;
+    prompt.scrollIntoView();
+    resizeInput();
+  }
+
+  function isCursorRow(row) {
+
+    var val = input.value;
+
+    if (!val)
+      return true;
+
+    var start = input.selectionStart;
+
+    if (start !== input.selectionEnd)
+      return false;
+
+    if (row === "first") {
+
+      var index = val.search(NEWLINE);
+      return index < 0 || index >= start;
+
+    } else if (row === "last") {
+
+      return start >= val.length - val.split(NEWLINE).pop().length;
+
+    } else {
+
+      return false;
+    }
+  }
+
+  function onKeyDown(evt) {
+    // CTL-C, ESC
+    if (evt.ctrlKey && evt.keyCode === 67 || evt.keyCode === 27) {
+      abort();
+      evt.preventDefault();
+      return;
+    }
+
+    // Up arrow
+    if (evt.keyCode === 38 && isCursorRow("first")) {
+      setInputValue(history.back(input.value));
+      evt.preventDefault();
+      return;
+    }
+
+    // Down arrow
+    if (evt.keyCode === 40 && isCursorRow("last")) {
+      setInputValue(history.forward(input.value));
+      evt.preventDefault();
+      return;
+    }
+  }
+
+  function onClick() {
+    if (!window.getSelection || window.getSelection().isCollapsed)
+      input.focus();
+  }
+
+  function onPaste() {
+    resizeInput();
+  }
+
+  function createHidden() {
+    var e = document.createElement(input.nodeName);
+    e.className = "hidden";
+    e.rows = 1;
+    input.parentNode.appendChild(e);
+    return e;
+  }
+
+  function loadFromHash() {
+    var m = /____(.+)/.exec(window.location.hash);
+
+    if (m) {
+      var program = decodeURIComponent(m[1]);
+      runProgram(program)
+      //input.value = decodeURIComponent(m[1]);
+      //resizeInput();
+    }
+  }
+
+  function runProgram(program) {
+    program.split('\n').forEach(function(line){
+      input.value = line;
+      replRun();
+      resizeInput();
+    })
+  }
+
+  function generateLink() {
+    function isNotCommand(text) {return text.charAt(0) !== ".";}
+    function innerText(el) {return el.innerText.trim()}
+
+    var out = [].map.call(elems("div.echo"), innerText).filter(isNotCommand).join("\n");
+    console.log(out);
+    out = "____" + encodeURIComponent(out);
+
+    window.location.hash = out;
+
+    return window.location.toString().replace(/#[\s\S]*/, "") + "#" + out;
+  }
+
+  var terminal = elem(".es6repl"),
+    input = elem(".terminal-input"),
+    hidden = createHidden(),
+    prompt = elem(".es6repl div.input-line span"),
+    bufferedInput = "";
+
+  terminal.addEventListener("click", onClick, false);
+  input.addEventListener("keypress", onKeyPress, false);
+  input.addEventListener("keydown", onKeyDown, false);
+  input.addEventListener("paste", onPaste, false);
+
+  input.focus();
+
+  window.print = function (str) {
+    addLine(str + "")
+  };
+
+  if (window.console) {
+    var consoleLog = window.console.log;
+    window.console.log = function (arg) {
+      window.print(arg);
+      consoleLog.apply(this, arguments);
+    };
+  }
+
+  loadFromHash();
+
+};
+
